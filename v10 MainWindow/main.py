d1cafea0779666c37cd7732121a4323ada5457e3
ImGreatest <ImGreatest@users.noreply.github.com>
2023-11-07 16:18:18 +0300

    created color class

--- v10 MainWindow/main.py
+++ v10 MainWindow/main.py
@@ -11,6 +11,7 @@ from matplotlib.figure import Figure
 matplotlib.use('Qt5Agg')
 
 
+
 class MplCanvas(FigureCanvas):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         self.color = Color()
